0ed4e90e370fa3cbf5004734ef449725d0511ee2
Xavier Paredes <jparedes@quantumbit.mx>
2018-01-02 00:58:12 -0700

    Modificando la clase cola

--- cola.h
+++ cola.h
@@ -1,30 +1,26 @@
 #ifndef COLA_H
 #define COLA_H
+#include "Nodo.h"
 
-    #include "Nodo.h"
-
-    struct caja{
-        Nodo* nodo;
-        caja* siguiente;
-    };
-
-    class Cola
-    {
-        caja* primero;
-        caja* ultimo;
-        int cuantos;
-
-        public:
-            Cola();
-            ~Cola();
-            void nuevo();
-            void limpiar();
-            void agregar(Nodo*);
-            Nodo* sacar();
-            void pintar();
-            bool estaVacia(){return cuantos==0;}
-
+struct Caja {
+    Nodo* nodo;
+    Caja* siguiente;
 };
 
-#endif // COLA_H
+class Cola {
+    private:
+        Caja* primero, ultimo;
+        int cuantos;
 
+    public:
+        Cola();
+        ~Cola();
+        void nuevo();
+        void limpiar();
+        void agregar(Nodo*);
+        Nodo* sacar();
+        void pintar();
+        bool estaVacia(){ return cuantos == 0; }
+};
+
+#endif
